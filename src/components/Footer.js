7d9644689b580dad6bdb6417f6af77868b1d274d
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-11-01 18:41:12 -0700

    footer/contact tweaks in Footer.js, adding more to app.css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ export const Footer = () => {
     return (
         <div className="container">
             <h2 id="contact-header">Get in touch!</h2>
+            <p> CONTACT FORM TO GO HERE </p>
+            <SocialIcon className="social-icon" target="_blank" url="mailto:kbienia@uw.edu" />
             <SocialIcon className="social-icon" target="_blank" url="https://github.com/kjnb-0" />
             <SocialIcon className="social-icon" target="_blank" url="https://www.linkedin.com/in/kjnielsen/" />
         </div>
